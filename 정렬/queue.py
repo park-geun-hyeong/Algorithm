ba1e05d78ef1004f741c58fe1ad03b1f2b0489ac
park-geun-hyeong <park30908@gmail.com>
2021-09-07 01:58:39 +0900

    queue 'backjoon 18258'

---
+++ 정렬/queue.py
@@ -0,0 +1,40 @@
+from collections import deque
+import sys
+
+n = int(input())
+queue = deque()
+
+for _ in range(n):
+    i = sys.stdin.readline().split()
+
+    if i[0] == 'push':
+        queue.append(i[1])
+
+    elif i[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print('-1')
+
+    elif i[0] == 'size':
+        print(len(queue))
+
+    elif i[0] == 'empty':
+        if queue:
+            print('0')
+        else:
+            print('1')
+
+    elif i[0] == 'front':
+        if queue:
+            print(queue[0])
+
+        else:
+            print('-1')
+
+    elif i[0] == 'back':
+        if queue:
+            print(queue[-1])
+
+        else:
+            print('-1')
