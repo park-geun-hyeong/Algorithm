2036e36a1177927b14dff9dffd4c79d8e1055a19
park <park30908@gmail.com>
2021-09-19 18:01:39 +0900

    baekjoon_11286

---
+++ Queue_Stack/baekjoon_11286.py
@@ -0,0 +1,27 @@
+import heapq
+import sys
+
+def solution(x):    
+    
+    arr=[]
+    for i in x:
+        if i > 0 :
+            heapq.heappush(arr, (i, i))
+        
+        elif i < 0:
+            heapq.heappush(arr, (-i, i))
+            
+        else:
+            try:
+                print(heapq.heappop(arr)[1])        
+                      
+            except IndexError:
+                print('0')
+            
+
+if __name__ == '__main__':
+    n = int(input())  
+    x = [int(sys.stdin.readline()) for _ in range(n)]    
+    solution(x)
+    
+    
