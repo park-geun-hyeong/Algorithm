304bdf4f9b4f540017a38884109f7a37244c5814
park <park30908@gmail.com>
2022-01-04 14:05:41 +0900

    permutation

---
+++ BackTracking/baekjoon_15649.py
@@ -0,0 +1,17 @@
+from itertools import permutations
+
+def permute(n,m):
+    return list(permutations(range(1, n+1), m))
+    
+if __name__ == "__main__":
+    n,m = tuple(map(int, input().split()))
+    for i in permute(n,m):
+        if m>1:
+            for j in i:
+                print(j, end=' ')
+            print()
+        else:  
+            print(i[0])
+        
+    
+    
