ebf03de47d26b9775c41afb307709638e9e616be
park <park30908@gmail.com>
2022-01-07 23:37:38 +0900

    combiniation BackTracking

---
+++ BackTracking/baekjoon_15650_BT.py
@@ -0,0 +1,23 @@
+def dfs(depth):
+    if depth == m:
+        print(' '.join(map(str, solve)))
+        return
+    
+    else: ## pruning & DFS
+        for i in range(1, n+1):
+            if len(solve) == 0:
+                solve.append(i)
+                dfs(depth + 1)
+                solve.pop()
+            elif i > solve[-1]:
+                solve.append(i)
+                dfs(depth + 1)
+                solve.pop()
+            else:
+                continue
+                       
+if __name__ == "__main__":
+    n,m = tuple(map(int, input().split()))
+    solve = []
+    dfs(0)
+    
