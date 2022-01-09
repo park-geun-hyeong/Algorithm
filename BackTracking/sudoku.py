58dd7896ccc95f4fd54f4a8deebc2eb466280f42
park <park30908@gmail.com>
2022-01-09 19:32:42 +0900

    baekjoon 2580 sudoku

--- BackTracking/sudoku.py
+++ BackTracking/sudoku.py
@@ -1,77 +1,55 @@
 import sys
-from itertools import product
 read= sys.stdin.readline
 
-def with_Row(row):
-    if row<3:
-        with_row = [0,1,2]
-    elif row<6:
-        with_row = [3,4,5]
-    else: 
-        with_row = [6,7,8]
-    
-    return with_row
+def check_row(i, row):
+    for j in range(9):
+        if i == graph[row][j]:
+            return False
+    return True
 
-def with_Col(col):
-    if col<3:
-        with_col = [0,1,2]
-    elif col<6:
-        with_col = [3,4,5]
-    else: 
-        with_col = [6,7,8]
-    
-    return with_col
+def check_col(i,col):
+    for j in range(9):
+        if i == graph[j][col]:
+            return False
+    return True
 
 def check_square(i, row, col):
+    nrow = (row // 3) * 3
+    ncol = (col // 3) * 3
     
-    with_row = with_Row(row)
-    with_col = with_Col(col)
+    for k in range(3):
+        for j in range(3):
+            if i == graph[nrow + k][ncol + j]:
+                return False
+    return True
+
+state = False
+def dfs(idx):
     
-    loc = tuple(product(with_row,with_col))
+    global state
     
-    square = [] 
-    for r,c, in loc:
-        square.append(graph[r][c])
-    
-    if i not in square:
-        return True
-    else:
-        return False
-           
-        
-def dfs(depth):
-    
-    if depth == 10:
+    if state:
         return
     
-    for col in range(9):      
-        for row in range(depth):
-            if graph[row][col] == 0:
-                for i in range(1, 10):
-                    if (i in ROW[row]) or (i in COL[col]) or (check_square(i,row,col) == False):
-                        continue
-                   
-                    else:
-                        graph[row][col] = i
-                        ROW[row][col] = i
-                        COL[col][row] = i
-                        dfs(depth + 1)
-            else: 
-                continue
-                
+    if idx == len(zero_position):
+        for i in graph:
+            print(' '.join(map(str, i)))
+            state = True
+        return 
+        
+    for i in range(1, 10):
+        row, col = zero_position[idx]
+        if check_row(i,row) and check_col(i,col) and check_square(i,row,col):
+            graph[row][col] = i
+            dfs(idx + 1)
+            graph[row][col] = 0 # 재귀안에서 답이 없을경우 초기화 한 뒤 다음 interation으로
+            
 if __name__ == "__main__":
     
-    graph = [list(map(int, input().split())) for _ in range(9)]
-    
-    ROW = graph
-    COL = [[] for _ in range(9)] # transpose
-    for i in ROW:
+    graph = [list(map(int, read().split())) for _ in range(9)]           
+    zero_position = []
+    for i in range(9):
         for j in range(9):
-            COL[j].append(i[j])
-             
-    dfs(1)
-    print()
-    for i in graph:
-        print(' '.join(map(str, i)))
-    
-
+            if graph[i][j] == 0:
+                zero_position.append([i,j])
+    dfs(0)
