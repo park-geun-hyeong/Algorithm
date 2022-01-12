6f55e9e661792ea1cd4892215d3fae5ce7fb3d1d
park <park30908@gmail.com>
2022-01-12 23:22:10 +0900

    once again

--- BackTracking/baekjoon_14889.py
+++ BackTracking/baekjoon_14889.py
@@ -2,35 +2,32 @@ import sys
 from itertools import combinations
 read = sys.stdin.readline
 
+def cal_team(member:set):
+    Sum = 0
+    for i in member:
+        for j in member:
+            Sum+=graph[i][j]
+    
+    return Sum
+
 def solution(graph):
-    ans = []
+    global MIN
     
     case = list(combinations(range(n), n//2))
     
     for i in case:
-        SUM1 = 0
-        SUM2 = 0
-        
         another = set(range(n)) - set(i)
-        
-        for j in i:
-            for k in i:
-                SUM1 += graph[j][k]
-        
-        for j in another:
-            for k in another:
-                SUM2 += graph[j][k]
-                                
-        ans.append(abs(SUM1 - SUM2))
-                       
-    return ans
+        Sub = cal_team(set(i)) - cal_team(another)
+        if abs(Sub) < MIN:
+            MIN = abs(Sub)
 
 if __name__ == "__main__":
     n = int(input())
     assert (n%2==0) and (4<=n<=20) 
     
-    graph = [list(map(int, read().split())) for _ in range(n)]
-    ans = solution(graph)
-    print(min(ans))
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    MIN = 10e9
+    solution(graph)
+    print(MIN)
     
     
