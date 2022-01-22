e48938aebcbe9216f005677a9f7210e3b484c35e
park <park30908@gmail.com>
2022-01-22 19:31:19 +0900

    이코테 구현 6번 - 카카오 2020 신입공채

--- Implementation/Pil_Bow.py
+++ Implementation/Pil_Bow.py
@@ -1,85 +1,35 @@
-def point_change(n,x,y):
-    return (n-y, x)
-
-def point_change2(n,row, col):
-    return (col, n-row)
-    
-def verticle_check(graph, b,  x, y):
-    
-    n = len(graph) - 1
-    row,col = point_change(n,x,y)
-    
-    
-    if b == 1:
-        if y == 0  or graph[row+1][col] == 1 or graph[row][col-1] == 2 or graph[row-1][col-1] == 2: 
-            return True
-        
-        elif row<1 or col<0 or col>n:
-            return False
+def possible(answer):    
+    for x,y, state in answer:
+        if state == 0:
+            if y==0 or [x,y-1,0] in answer or [x, y ,1] in answer or [x-1,y,1] in answer:
+                continue
+            else:
+                return False
             
-               
-    else:
-        if graph[row-1][col] == 2 and graph[row-1][col-1] == 2:
-            return True
-              
-        elif graph[row-1][col] == 2 or graph[row-1][col] == 1:
-            return False
-        
         else:
-            return True
-
-
-def horizontal_check(graph, b, x, y):
+            if [x,y-1,0] in answer or [x+1,y-1,0] in answer or ([x-1,y,1] in answer and [x+1,y,1] in answer):
+                continue
+            else:
+                return False
     
-    n = len(graph) - 1
-    row,col = point_change(n,x,y)
-    
-    if b == 1:
-        if  y == 0 or col >= n:
-            return False
-        
-        else:
-            if graph[row+1][col] == 1 or graph[row+1][col+1] == 1 or (graph[row][col+1] == 2 and graph[row][col-1] == 2):
-                return True
-        
-    else:
-        if graph[row][col+1] == 1 or (graph[row][col-1]==2 and graph[row][col+1] == 2):
-            return False
-        else:
-            return True
-        
+    return True
+
 def solution(n, build_frame):
     
-    graph = [[0]*(n+1) for _ in range(n+1)]
-    result = []
+    answer = [] 
     
     for x,y,a,b in build_frame:
-        if a==0:
-            if verticle_check(graph, b, x, y):
-                row,col = point_change(n,x,y)
-                if b==1:
-                    graph[row][col] += 1
-                else:
-                    graph[row][col] -= 1
-                    
+        if b == 0:
+            answer.remove([x,y,a])
+            if possible(answer) == False:
+                answer.append([x,y,a])
+                               
         else:
-            if horizontal_check(graph,b,x,y):
-                row,col = point_change(n,x,y)
-                if b==1:
-                    graph[row][col] += 2
-                else:
-                    graph[row][col] -= 2
-       
-    for i in range(n+1):
-        for j in range(n+1):
-            if graph[i][j] == 1:
-                x,y = point_change2(n,i,j)
-                result.append([x,y,0])
-            elif graph[i][j] == 2:
-                x,y = point_change2(n,i,j)
-                result.append([x,y,1])
-            
-    return sorted(result) 
+            answer.append([x,y,a])
+            if possible(answer) == False:
+                answer.remove([x,y,a])
+                
+    return sorted(answer)
 
 
 if __name__ == "__main__":
