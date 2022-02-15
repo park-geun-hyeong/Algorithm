c6a2c235741a93e1a5f94a2e65356fb5926e3639
park <park30908@gmail.com>
2022-02-16 01:46:43 +0900

    1차 인증평가

--- softeer/robot_path.py
+++ softeer/robot_path.py
@@ -11,29 +11,27 @@ def check_direc(direc, i):
             return 'L'
         elif i == 3:
             return 'R'
-        else: return False
     
     if direc == 1:
         if i == 2:
             return 'R'
         elif i == 3:
             return 'L'
-        else: return False
         
     if direc == 2:
         if i == 0:
             return 'R'
         elif i == 1:
             return 'L'
-        else: return False
         
     if direc == 3:
         if i == 0:
             return 'L'
         elif i == 1:
             return 'R'
-        else: return False
-        
+
+    return None
+
 def solution(h,w,graph):
     drow = [1,-1,0,0]
     dcol = [0,0,1,-1]
@@ -66,12 +64,65 @@ def solution(h,w,graph):
                             direction = '<'
                             
                     start_point.append([row,col,direction])
-
+    
     using_case = start_point[0]
-
+    
     q = deque([[using_case[0], using_case[1]]])
     move = ''
     direc = 0
     if using_case[2] == 'v':
         direc = 0
+    elif using_case[2] == '^':
+        direc = 1
+    elif using_case[2] == '>':
+        direc = 2
+    elif using_case[2] == '<':
+        direc = 3
+    
+    dr=[1,-1,0,0]
+    dc=[0,0,1,-1]
+    
+    drow = [2,-2,0,0]
+    dcol = [0,0,2,-2]
+    
+    while q :
+        row, col = q.popleft()
+        mat[row][col] = 1
+        for i in range(4):
+            nrow = row + drow[i]
+            ncol = col + dcol[i]
+            
+            nr = row+dr[i]
+            nc = col+dc[i]
+            
+            if 0<=nrow<h and 0<=ncol<w and mat[nrow][ncol] == 0 and mat[nr][nc] == 0:
+                if check_direc(direc, i) == 'G':
+                    move+='A'
+                    q.append([nrow,ncol])
+                    mat[nr][nc] = 1
+
+                   
+                elif  check_direc(direc, i) == 'R':
+                    move+='RA'
+                    q.append([nrow,ncol])
+                    mat[nr][nc] = 1
+                    direc = i
+
+                
+                elif check_direc(direc, i) == 'L':
+                    move+='LA'
+                    q.append([nrow,ncol])
+                    mat[nr][nc] = 1
+                    direc = i
+
+    return using_case, move
+
+if __name__ == "__main__":
+    h,w = list(map(int, read().split()))
+    graph = [list(map(str,read().rstrip())) for _ in range(h)]
+    
+    using_case, move = solution(h,w,graph)
+    print(using_case[0]+1, using_case[1]+1)
+    print(using_case[2])
+    print(move)
 
