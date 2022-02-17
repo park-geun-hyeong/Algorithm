b3c4582527a463a2deffa9243f98bae83f189825
park <park30908@gmail.com>
2022-02-17 15:38:33 +0900

    [인증평가(1차) 기출] 차세대 지능형 교통시스템 BFS 풀이

--- softeer/translation_system.py
+++ softeer/translation_system.py
@@ -1,85 +1,96 @@
-# https://softeer.ai/practice/info.do?eventIdx=1&psProblemId=580&sw_prbl_sbms_sn=43796
+# https://softeer.ai/practice/info.do?eventIdx=1&psProblemId=580&sw_prbl_sbms_sn=43814
+'''
+direction 2 : 아래 -> 위
+direction 3 : 오른 -> 왼
+direction 0 : 위 -> 아래
+direction 1 : 왼 -> 오른
+
+'''
+from collections import deque
 import sys
 read = sys.stdin.readline
 
-def is_true(n,nrow,ncol):
+def is_in(n,nrow,ncol):
     if 0<=nrow<n and 0<=ncol<n:
         return True
     return False
 
+def return_direc(i):
+    if i == 0:
+        return 0
+    elif i == 1:
+        return 2   
+    elif i == 2:
+        return 1       
+    elif i == 3:
+        return 3
+    
 def next_direction(direction, graph, row,col, time):
     order = time%4
-    state = graph[row][col][order]
+    state = graph[row][col][order%4]
     n = len(graph)
     move = [[1,0],[-1,0],[0,1],[0,-1]]
     next_step = []
     signal = [[0],
               [0,1,2], [1,2,3],[0,1,3],[0,2,3],
               [1,2],[1,3],[0,3],[0,2],
-              [0,3], [1,2],[1,3],[0,3]]
+              [0,2], [1,2],[1,3],[0,3]]
     
     
-    if direction != (state%4):
-        return False
-    
-    direc = 0
-    for i in signal[state]:
-        nrow = row + move[i][0]
-        ncol = col + move[i][1]
-        if is_true(n, nrow, ncol):
-            if i == 0:
-                direc = 0
-            elif i == 1:
-                direc = 2
-            elif i == 2:
-                direc = 1
-            elif i == 3:
-                direc = 3
+    if direction == (state%4):
+        for i in signal[state]:
+            nrow = row + move[i][0]
+            ncol = col + move[i][1]
+            if is_in(n, nrow, ncol):
+                direc = return_direc(i)
+                next_step.append([nrow, ncol, direc, time+1])
+            else:
+                continue
                 
-            next_step.append([nrow, ncol, direc])
-    
-    if len(next_step) == 0:
-        return False
-    return next_step
-            
-ans = [[0,0]]
-def recursive(direction, graph, row,col, time):
-        
-    global ans
-    global t
-    
-    if time == t:
-        return 
+        if len(next_step) == 0:
+            return False
+        return next_step
+    return False
 
-    next_step = next_direction(direction, graph, row,col, time)
-    #print(time, next_step)
-    if next_step == False:
-        return
+def bfs(graph,n,t):
+    check = [[0]*n for _ in range(n)]
+    time = 0
+    direction = 2
+    q = deque([[0,0, time, direction]]) 
+    check[0][0] = 1
+    cnt = 1
+    ans = set([0,0])
     
-    for nrow, ncol, direc in next_step:
-        if [nrow,ncol] not in ans:
-            ans.append([nrow, ncol])
-        recursive(direc, graph, nrow, ncol, time + 1)
+    while q:
+        row, col, time, direction = q.popleft()
+        
+        if time == t:
+            break
+            
+        next_step = next_direction(direction, graph, row,col, time)
+        if next_step != False:
+            for nrow, ncol, direc, T in next_step:
+                q.append([nrow,ncol,T,direc])
+                if check[nrow][ncol] == 0:
+                    check[nrow][ncol] = 1
+                    cnt += 1
+                
+        
+    return cnt
 
 if __name__ == "__main__":
     n,t = map(int, read().split())
     if n == 1:
-        print(n)
+        print(0)
         sys.exit()
 
-    graph = [[0]*3 for _ in range(n)]
+    graph = [[0]*n for _ in range(n)]
       
     for row in range(n):
         for col in range(n):
             info = list(map(int, read().split()))
             graph[row][col] = info
             
-    start_direction = 2
-    start_time = 0 
-    start_row = 0
-    start_col = 0
-    
-    recursive(start_direction, graph, start_row, start_col, start_time)
-    #print(ans)
-    print(len(ans))
+    print(bfs(graph,n,t))
 
+    
